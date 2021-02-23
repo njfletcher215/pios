b1b9fb479a1d3bd678c1a35af598ad993911f45d
njfletcher215 <njfletcher215@gmail.com>
2021-02-23 16:23:53 -0600

    added gpio.c and blinky functionality to kernel_main.c

---
+++ src/gpio.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include <stdio.h>
+
+void led_init();
+void led_on();
+void led_off();
+
+void led_init() {
+	uint32_t *gpfsel4 = (uint32_t *)0xFE200010;
+	unsigned int andMask = 0xFFFFFE3F;	// 1111 1111 1111 1111 1111 1100 0111 1111
+	*gpfsel4 = *gpfsel4 & andMask;
+	*gpfsel4 = *gpfsel4 | (1 << 6);
+	return;
+}
+
+void led_on() {
+	uint32_t *gpset1 = (uint32_t *)0xFE200020;
+	*gpset1 = (1 << 10);
+}
+
+void led_off() {
+	uint32_t *gpclr1 = (uint32_t *)0xFE20002C;
+	*gpclr1 = (1 << 10);
+}
+
+void delay() {
+	unsigned int x = 0;
+	for (x; x < 100000; x++) {
+		asm("NOP");
+	}
+}
+
--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,17 @@
+#include "gpio.c"
+
 extern char __bss_start;
 extern char __bss_end;
 extern char __bss_size;
 
-int global ;
+int global;
+
+void clear_bss();
+void kernel_main();
+void led_init();
+void led_on();
+void led_off();
+void sleep(unsigned long);
 
 void clear_bss() {
 
@@ -17,6 +26,11 @@ void clear_bss() {
 
 void kernel_main() {
 	clear_bss();
+	led_init();
 	while (1) {
+		led_on();
+		delay();
+		led_off();
+		delay();
 	}
 }
